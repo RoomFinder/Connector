08655095f691bc298145ef70a5bd731d4c9adcae
Pavel Samarkin <psamarkin@mirantis.com>
2015-10-27 13:32:24 -0700

    ListView should not be hidden behind Panel

--- FindFreeRoom/MainForm.Designer.cs
+++ FindFreeRoom/MainForm.Designer.cs
@@ -54,11 +54,13 @@
 			// 
 			// choicesListView
 			// 
-			this.choicesListView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.choicesListView.Location = new System.Drawing.Point(0, 0);
+			this.choicesListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.choicesListView.Location = new System.Drawing.Point(0, 70);
 			this.choicesListView.MultiSelect = false;
 			this.choicesListView.Name = "choicesListView";
-			this.choicesListView.Size = new System.Drawing.Size(353, 154);
+			this.choicesListView.Size = new System.Drawing.Size(353, 84);
 			this.choicesListView.TabIndex = 1;
 			this.choicesListView.UseCompatibleStateImageBehavior = false;
 			this.choicesListView.View = System.Windows.Forms.View.List;
