9a4b14e0ac0db2c91b6e9b70e117c07d40af3899
Pavel Samarkin <psamarkin@mirantis.com>
2015-10-27 16:11:23 -0700

    File server added

--- ConnectorWebService/ServiceHost.cs
+++ ConnectorWebService/ServiceHost.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using ConnectorWebService.Web;
 
 namespace ConnectorWebService
 {
 	public class ServiceHost : IDisposable
 	{
 		private static readonly Uri ServiceUri = new Uri("http://0.0.0.0:18799/api");
+		private static readonly Uri WebAppUri = new Uri("http://0.0.0.0:18799");
 
 		internal static ILog Log;
 		internal static IStatusMonitor StatusMonitor;
 		private readonly WebServiceHost _host;
+		private readonly WebServiceHost _webAppHost;
 		private readonly CancellationTokenSource _gcCancellation = new CancellationTokenSource();
 
 		public ServiceHost(ILog log, IStatusMonitor statusMonitor)
@@ -24,13 +28,25 @@ namespace ConnectorWebService
 			// TODO: Enable HTTPS - http://stackoverflow.com/questions/14933696
 			_host.AddDefaultEndpoints()[0].Binding = new WebHttpBinding();
 			_host.Description.Endpoints[0].Behaviors.Add(new WebHttpBehavior { HelpEnabled = true });
+
+			_webAppHost = new WebServiceHost(typeof(WebServer), WebAppUri);
+			_webAppHost.AddDefaultEndpoints()[0].Binding = new WebHttpBinding();
+			_webAppHost.Description.Endpoints[0].Behaviors.Add(new WebHttpBehavior());
 		}
 
 		public void Start()
 		{
 			try
 			{
-				Task.Run(() => _host.Open()).Wait();
+				try
+				{
+					Task.Run(() => _host.Open()).Wait();
+					Task.Run(() => _webAppHost.Open()).Wait();
+				}
+				catch (AggregateException ex)
+				{
+					throw ex.InnerExceptions.FirstOrDefault() ?? ex;
+				}
 				Task.Run(() => SessionManager.GarbageCollectorAsync(_gcCancellation.Token));
 				StatusMonitor?.SetOnline();
 				Log?.AddMessage("Service successfully started at {0}", ServiceUri);
@@ -48,6 +64,7 @@ namespace ConnectorWebService
 			{
 				_gcCancellation.Cancel();
 				_host.Close();
+				_webAppHost.Close();
 				StatusMonitor?.SetOffline();
 			}
 			catch (Exception ex)
---
+++ ConnectorWebService/Web/WebServer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace ConnectorWebService.Web
+{
+
+	[ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]
+	[ServiceContract]
+	public class WebServer
+	{
+		public const string Name = "Web";
+		private static readonly Assembly CurrentAssembly = typeof(WebServer).Assembly;
+
+		private static readonly NameValueCollection MimeDictionary = new NameValueCollection
+		{
+			{".css" , "text/css"},
+			{".html", "text/html"},
+			{".js" , "text/javascript"},
+			{".gif" , "image/gif"},
+		};
+
+		[OperationContract]
+		[WebGet(UriTemplate = "*")]
+		[OperationBehavior(AutoDisposeParameters = true)]
+		public Stream GetFile()
+		{
+			var context = WebOperationContext.Current;
+			var filePathParts = context.IncomingRequest.UriTemplateMatch.WildcardPathSegments;
+			if (!filePathParts.Any())
+			{
+				filePathParts.Add("index.html");
+			}
+			string filePath = filePathParts.Aggregate(Name, Path.Combine);
+
+			Stream result = TryLoadFile(filePath) ?? TryLoadResource(filePath);
+
+			if (result == null)
+			{
+				throw new WebFaultException<string>("File not found", HttpStatusCode.NotFound);
+			}
+
+			try
+			{
+				context.OutgoingResponse.ContentType = GetMimeType(Path.GetExtension(filePath));
+				//WCF will take care of calling Dispose() for file stream
+				return result;
+
+			}
+			catch (Exception e)
+			{
+				throw new WebFaultException<string>($"File cannot be read: {e.Message}", HttpStatusCode.InternalServerError);
+			}
+		}
+
+		private static Stream TryLoadResource(string path)
+		{
+			string resourcePath = "ConnectorWebService." + path.Replace('/', '.').Replace('\\', '.');
+			return CurrentAssembly.GetManifestResourceStream(resourcePath);
+		}
+
+		private static Stream TryLoadFile(string path)
+		{
+			if (!File.Exists(path))
+				return null;
+
+			try
+			{
+				return new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+			}
+			catch (Exception e)
+			{
+				throw new WebFaultException<string>($"File cannot be read: {e.Message}", HttpStatusCode.InternalServerError);
+			}
+		}
+
+		private static string GetMimeType(string ext)
+		{
+			return MimeDictionary[ext] ?? "application/octet-stream";
+		}
+	}
+}
