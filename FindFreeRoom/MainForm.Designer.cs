54d1a5eeeb29aa11444ad6574a112dbc08653fc1
Alexei Strots <alexei_strots@agilent.com>
2015-10-27 12:30:15 -0700

    Add setting and start integration with exchange

--- FindFreeRoom/MainForm.Designer.cs
+++ FindFreeRoom/MainForm.Designer.cs
@@ -28,9 +28,14 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.startButton = new System.Windows.Forms.Button();
 			this.choicesListView = new System.Windows.Forms.ListView();
 			this.reserveButton = new System.Windows.Forms.Button();
+			this.buttonPanel = new System.Windows.Forms.Panel();
+			this.timeLabel = new System.Windows.Forms.Label();
+			this.ticker = new System.Windows.Forms.Timer(this.components);
+			this.buttonPanel.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// startButton
@@ -39,44 +44,77 @@
 			this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.startButton.ForeColor = System.Drawing.Color.White;
-			this.startButton.Location = new System.Drawing.Point(2, 3);
+			this.startButton.Location = new System.Drawing.Point(3, 3);
 			this.startButton.Name = "startButton";
 			this.startButton.Size = new System.Drawing.Size(85, 61);
 			this.startButton.TabIndex = 0;
 			this.startButton.Text = "Search";
 			this.startButton.UseVisualStyleBackColor = false;
+			this.startButton.Click += new System.EventHandler(this.startButton_Click);
 			// 
 			// choicesListView
 			// 
-			this.choicesListView.Location = new System.Drawing.Point(2, 70);
+			this.choicesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.choicesListView.Location = new System.Drawing.Point(0, 0);
+			this.choicesListView.MultiSelect = false;
 			this.choicesListView.Name = "choicesListView";
-			this.choicesListView.Size = new System.Drawing.Size(278, 133);
+			this.choicesListView.Size = new System.Drawing.Size(284, 208);
 			this.choicesListView.TabIndex = 1;
 			this.choicesListView.UseCompatibleStateImageBehavior = false;
+			this.choicesListView.View = System.Windows.Forms.View.List;
 			// 
 			// reserveButton
 			// 
 			this.reserveButton.BackColor = System.Drawing.Color.LimeGreen;
+			this.reserveButton.Enabled = false;
 			this.reserveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
 			this.reserveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.reserveButton.ForeColor = System.Drawing.Color.White;
-			this.reserveButton.Location = new System.Drawing.Point(196, 3);
+			this.reserveButton.Location = new System.Drawing.Point(94, 3);
 			this.reserveButton.Name = "reserveButton";
 			this.reserveButton.Size = new System.Drawing.Size(84, 61);
-			this.reserveButton.TabIndex = 2;
+			this.reserveButton.TabIndex = 1;
 			this.reserveButton.Text = "Reserve";
 			this.reserveButton.UseVisualStyleBackColor = false;
+			this.reserveButton.Visible = false;
+			// 
+			// buttonPanel
+			// 
+			this.buttonPanel.Controls.Add(this.timeLabel);
+			this.buttonPanel.Controls.Add(this.startButton);
+			this.buttonPanel.Controls.Add(this.reserveButton);
+			this.buttonPanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.buttonPanel.Location = new System.Drawing.Point(0, 0);
+			this.buttonPanel.Name = "buttonPanel";
+			this.buttonPanel.Size = new System.Drawing.Size(284, 68);
+			this.buttonPanel.TabIndex = 3;
+			// 
+			// timeLabel
+			// 
+			this.timeLabel.AutoSize = true;
+			this.timeLabel.Location = new System.Drawing.Point(184, 9);
+			this.timeLabel.Name = "timeLabel";
+			this.timeLabel.Size = new System.Drawing.Size(0, 13);
+			this.timeLabel.TabIndex = 2;
+			// 
+			// ticker
+			// 
+			this.ticker.Enabled = true;
+			this.ticker.Interval = 1000;
+			this.ticker.Tick += new System.EventHandler(this.ticker_Tick);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 208);
-			this.Controls.Add(this.reserveButton);
+			this.Controls.Add(this.buttonPanel);
 			this.Controls.Add(this.choicesListView);
-			this.Controls.Add(this.startButton);
 			this.Name = "MainForm";
 			this.Text = "Find Free Room";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.buttonPanel.ResumeLayout(false);
+			this.buttonPanel.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -86,6 +124,9 @@
 		private System.Windows.Forms.Button startButton;
 		private System.Windows.Forms.ListView choicesListView;
 		private System.Windows.Forms.Button reserveButton;
+		private System.Windows.Forms.Panel buttonPanel;
+		private System.Windows.Forms.Label timeLabel;
+		private System.Windows.Forms.Timer ticker;
 	}
 }
 
--- FindFreeRoom/MainForm.cs
+++ FindFreeRoom/MainForm.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FindFreeRoom.ExchangeConnector;
+using FindFreeRoom.ExchangeConnector.Base;
 
 namespace FindFreeRoom
 {
 	public partial class MainForm : Form
 	{
+		private ExchangeConnector.ExchangeConnector _connector;
+		private LocationResolver _locations;
+
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				var props = Properties.Settings.Default;
+				_connector = new ExchangeConnector.ExchangeConnector(props.email);
+
+				string currentSite = props.currentSite;
+				_locations = new LocationResolver();
+				_locations.Load("locationMap.csv");
+				_connector.LocationFilter = _locations.OfSite(currentSite).ToArray(); // filter locations by site
+				_connector.Connect();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				Application.Exit();
+			}
+		}
+
+		private void startButton_Click(object sender, EventArgs e)
+		{
+			choicesListView.Items.Clear();
+			choicesListView.Refresh();
+
+			// TODO: need progress bar
+
+			var roomsNearby = _connector.GetFilteredRooms();
+			var roomsWithLocations = _locations.ResolveLocations(roomsNearby);
+
+			var results = _connector.GetAvaialility(roomsWithLocations).Select(
+				room => 
+					room.Availability == TimeInterval.Zero ? 
+						$"{room.Room.Name} is not available in the nearest future" : 
+						$"{room.Room.Name} is available from {room.Availability.Start} for {room.Availability.Duration.TotalMinutes} minutes").ToArray();
+
+			choicesListView.BeginUpdate();
+			foreach (var result in results)
+			{
+				choicesListView.Items.Add(result);
+			}
+			choicesListView.EndUpdate();
+		}
+
+		private void ticker_Tick(object sender, EventArgs e)
+		{
+			timeLabel.Text = DateTime.Now.ToShortTimeString();
 		}
 	}
 }
